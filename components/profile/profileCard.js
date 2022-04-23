bd5b0af993c482b9fb9931f5b6e42965e1fc1abd
AxonBlAzE <s.mulkutkar@somaiya.edu>
2022-04-23 22:28:37 +0530

    completed profile card

--- components/profile/profileCard.js
+++ components/profile/profileCard.js
@@ -1,11 +1,21 @@
 import styles from "../../styles/Profile.module.css";
-import { useRouter } from "next/router";
-import { useState, useEffect } from 'react';;
-import Nav from "../../components/Nav.js";
-import { useMoralis } from "react-moralis";
+import { useState, useEffect } from 'react';
 import { Typography, Container, Card, Grid, CardContent, TextField } from "@mui/material";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import { RemoveRedEye } from "@mui/icons-material";
+
 
 const ProfileCard = () => {
+    const [showAddress, setShowAddress] = useState("password");
+
+    const handleShowAddress = () => {
+        setShowAddress(showAddress === "password" ? "text" : "password");
+    };
+
     return ( 
         <div className={styles.wrapper}>
                <h1 style={{
@@ -21,34 +31,10 @@ const ProfileCard = () => {
                             height: "160px",
                             borderRadius: "50%",
                             margin: "0 auto",
-                            marginBottom: "10px",
+                            marginBottom: "20px",
                         }}/>
                         <div className="Details">
                             <Grid container spacing={5}>
-                                {/* <Grid xs={12} sm={6} item fullWidth>
-                                <h3 style={{
-                                    fontSize: "1.5rem",
-                                    fontWeight: "bold",
-                                    marginBottom: "10px",
-                                    color: "#606060",
-                                    border: "1px solid #606060",
-                                    borderRadius: "5px",
-                                }}>name</h3>
-                                </Grid>
-                                <Grid xs={12} sm={6} item>
-                                <p style={{
-                                    fontSize: "1.5rem",
-                                    fontWeight: "bold",
-                                    marginBottom: "10px",
-                                    color: "#606060",
-                                }}>email</p>
-                                </Grid>
-                                <Grid xs={12} sm={12} item>
-                                <p style={{
-                                    fontSize: "1.25rem",
-                                    color: "#606060",
-                                }}>walletaddress</p>
-                                </Grid> */}
                                 <Grid xs={12} sm={6} item>
                                     <TextField label="Username" variant="outlined" value={"Axon"} fullWidth inputMode="none"/>
                                 </Grid>
@@ -56,8 +42,18 @@ const ProfileCard = () => {
                                     <TextField label="Email" variant="outlined" value={"anon@gmail.com"} fullWidth/>
                                 </Grid>
                                 <Grid xs={12} sm={12} item>
-                                    <TextField label="Wallet Address" variant="outlined" value={"0x0ADWDJWAFHI2U87EI2UAUF7W"} fullWidth/>
+                                    <TextField label="Wallet Address" variant="outlined" value={"0x0ADWDJWAFHI2U87EI2UAUF7W"} type={showAddress} fullWidth
+                                    InputProps={{
+                                        endAdornment:
+                                          <InputAdornment position="end">
+                                              <IconButton aria-label="upload picture" component="span" onClick={() => handleShowAddress()}>
+                                                < RemoveRedEye />
+                                              </IconButton>
+                                          </InputAdornment>,
+                                      }}
+                                    />
                                 </Grid>
+                                {/* <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel> */}
                             </Grid>
                         </div>
                 </div> 
