1b3db92afdbff4cfab190f95e9d1464b4c67a90c
AxonBlAzE <s.mulkutkar@somaiya.edu>
2022-04-24 22:47:14 +0530

    pls work

--- components/profile/campaignCard.js
+++ components/profile/campaignCard.js
@@ -11,7 +11,7 @@ const Item = styled(Paper)(({ theme }) => ({
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
-    border: "None",s
+    border: "None",
   }));
 
 
