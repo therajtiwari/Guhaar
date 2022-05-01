ddd5fabd993babfc8872552d108257ef8671557b
Raj <therajtiwari254@gmail.com>
2022-05-02 01:02:01 +0530

    feat:add loader on profile

--- components/Loading.js
+++ components/Loading.js
@@ -3,28 +3,28 @@ import { BounceLoader } from "react-spinners";
 
 const Loading = () => {
 
-    const override = css`
+  const override = css`
     display: block;
   margin: 0 auto;
   border-color: red;`
 
-    return ( 
-        <div
-        className="loader"
-        style={{
-          minHeight: "80vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <BounceLoader
-          color="#6f49fd"
-          loading={true}
-          css={override}
-          size={120}
-        />
-      </div> );
+  return (
+    <div
+      className="loader"
+      style={{
+        minHeight: "80vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <BounceLoader
+        color="#6f49fd"
+        loading={true}
+        css={override}
+        size={120}
+      />
+    </div>);
 }
- 
+
 export default Loading;
